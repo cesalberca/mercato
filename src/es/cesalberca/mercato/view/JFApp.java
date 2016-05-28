14cf199bbd86436e4e411240007f38f708779dad
César Alberca <cesalberca@gmail.com>
2016-05-28 20:19:48 +0200

    Probando gitignore

--- src/es/cesalberca/mercato/view/JFApp.java
+++ src/es/cesalberca/mercato/view/JFApp.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package es.cesalberca.mercato.view;
 
 import es.cesalberca.mercato.controller.database.DatabaseConnector;
@@ -18,7 +13,6 @@ import javax.swing.JOptionPane;
  * @author César Alberca
  */
 public class JFApp extends javax.swing.JFrame {
-
     JPApp jpa;
     private Shop shop;
   
